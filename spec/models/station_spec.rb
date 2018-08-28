456e6a7eecb26344979433de0e199abb8218176b
lislis <mail@lislis.de>
2018-08-28 13:19:26 +0200

    update model tests

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Station, type: :model do
   it { should belong_to(:walk) }
-  it { should have_many(:subjects) }
-  it { should belong_to(:user)}
+  it { should have_many(:subjects).dependent(:destroy) }
+  it { should belong_to(:user) }
 
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:description) }
--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Subject, type: :model do
   it { should belong_to(:station) }
-  it { should have_many(:pages) }
+  it { should have_many(:pages).dependent(:destroy) }
   it { should belong_to(:user)}
 
   it { should validate_presence_of(:name) }
