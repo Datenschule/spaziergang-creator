66619db5585c91101d011f392fe35d2a32b68c4c
lislis <mail@lislis.de>
2018-09-03 13:02:55 +0200

    add static controller tests

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -2,4 +2,31 @@ require 'rails_helper'
 
 RSpec.describe StaticController, type: :controller do
 
+  describe 'GET index' do
+    it 'renders index template' do
+      get :index, params: { locale: :de }
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET onboarding' do
+    it 'renders onboarding template' do
+      get :onboarding, params: { locale: :de }
+      expect(response).to render_template(:onboarding)
+    end
+  end
+
+  describe 'GET impressum' do
+    it 'renders impressum template' do
+      get :impressum, params: { locale: :de }
+      expect(response).to render_template(:impressum)
+    end
+  end
+
+  describe 'GET data_protection' do
+    it 'renders data_protection template' do
+      get :data_protection, params: { locale: :de }
+      expect(response).to render_template(:data_protection)
+    end
+  end
 end
