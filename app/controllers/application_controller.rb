a9682204b31538a4ca40b72fcd93a85dce22e2f3
lislis <mail@lislis.de>
2018-09-10 15:12:50 +0200

    add username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Response
   include ExceptionHandler
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
   def set_locale
@@ -22,5 +23,13 @@ class ApplicationController < ActionController::Base
 
   def bad_request
     render file: Rails.root.join('public/400.html'), status: 400
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature 'Login', type: :feature do
+  let!(:user) { create(:user, username: 'test', email: 'test@test.de', password: '1234567890abcde')}
+  scenario 'with email' do
+    visit root_path
+    click_link I18n.t('signin.label')
+    fill_in 'Login', with: 'test@test.de'
+    fill_in 'Password', with: '1234567890abcde'
+    click_button 'Sign in'
+    expect(page).to have_content("Ahoi, test@test.de!")
+  end
+
+  scenario 'with username' do
+    visit root_path
+    click_link I18n.t('signin.label')
+    fill_in 'Login', with: 'test'
+    fill_in 'Password', with: '1234567890abcde'
+    click_button 'Sign in'
+    expect(page).to have_content("Ahoi, test@test.de!")
+  end
+end
