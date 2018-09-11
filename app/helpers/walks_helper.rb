d81ee1c57abd65050389756936739e4da8210f5c
lislis <mail@lislis.de>
2018-09-11 15:05:09 +0200

    walks helper test

--- app/helpers/walks_helper.rb
+++ app/helpers/walks_helper.rb
@@ -1,13 +1,13 @@
 module WalksHelper
-  def walks_public_status_helper
+  def walks_public_status_helper(walk)
     walk_status = 'walk'
-    if @walk.stations.present? && @walk.stations.count >= 2
+    if walk.stations.present? && walk.stations.count >= 2
       walk_status = 'station'
-      if @walk.courseline.present?
+      if walk.courseline.present?
         walk_status = 'courseline'
-        if @walk.stations.first.subjects.present?
+        if walk.stations.first.subjects.present?
           walk_status = 'subject'
-          if @walk.stations.first.subjects.first.pages.present?
+          if walk.stations.first.subjects.first.pages.present?
             walk_status = 'page'
           end
         end
